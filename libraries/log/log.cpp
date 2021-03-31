7827f970882959519bfcf6c800fe9ccdaeb36084
Steve Gerbino <steve@gerbino.co>
2021-03-31 14:19:22 -0400

    #1: Add util as a dependency, utilize the random alphanumeric utility function

--- libraries/log/log.cpp
+++ libraries/log/log.cpp
@@ -1,4 +1,5 @@
 #include <koinos/log.hpp>
+#include <koinos/util.hpp>
 
 #include <algorithm>
 #include <random>
@@ -113,24 +114,6 @@ public:
       s << ">: " << formatted_string << std::endl;
    }
 };
-
-inline std::string random_alphanumeric( std::size_t len )
-{
-   std::mt19937 generator( std::random_device{}() );
-   auto random_char = [&]() -> char
-   {
-      constexpr char charset[] =
-         "0123456789"
-         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-         "abcdefghijklmnopqrstuvwxyz";
-      constexpr std::size_t max_index = sizeof( charset ) - 1;
-      std::uniform_int_distribution<> distribution( 0, max_index );
-      return charset[ distribution( generator ) % max_index ];
-   };
-   std::string str( len, 0 );
-   std::generate_n( str.begin(), len, random_char );
-   return str;
-}
 
 void initialize_logging(
    const boost::filesystem::path& path,

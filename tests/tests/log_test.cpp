18b1c956220af394159aa0a17d1d95bc8a36344a
Steve Gerbino <steve@gerbino.co>
2021-04-01 01:57:53 -0400

    #1: Add log filtering test

--- tests/tests/log_test.cpp
+++ tests/tests/log_test.cpp
@@ -183,4 +183,69 @@ BOOST_AUTO_TEST_CASE( log_no_color_tests )
    boost::log::core::get()->remove_all_sinks();
 }
 
+BOOST_AUTO_TEST_CASE( log_filter_tests )
+{
+   BOOST_TEST_MESSAGE( "Testing log filtering" );
+   std::stringstream stream;
+   auto buf = std::cout.rdbuf();
+   std::cout.rdbuf( stream.rdbuf() );
+
+   std::vector< std::string > logtypes {
+       "<warning>",
+       "<error>",
+       "<fatal>",
+       "<unknown>"
+   };
+
+   auto temp = std::filesystem::temp_directory_path() / "log";
+   std::filesystem::create_directory( temp );
+   koinos::initialize_logging( temp, "log_test", "9abcd", koinos::log_level::warning, "log_test_no_color_%3N.log", false /* no color */ );
+
+   LOG( trace )   << "test";
+   LOG( debug )   << "test";
+   LOG( info )    << "test";
+   LOG( warning ) << "test";
+   LOG( error )   << "test";
+   LOG( fatal )   << "test";
+
+   // We go around our macro in order to invoke an unknown log level
+   BOOST_LOG_SEV(::boost::log::trivial::logger::get(), boost::log::trivial::severity_level(10))
+      << boost::log::add_value("Line", __LINE__)
+      << boost::log::add_value("File", boost::filesystem::path(__FILE__).filename().string()) << "test";
+
+   auto file_path = temp / "log_test_no_color_000.log";
+   std::ifstream file( file_path.string() );
+   BOOST_REQUIRE( file.is_open() );
+
+   std::vector< std::string > log_lines;
+   std::string line;
+
+   while ( std::getline( file, line ) )
+      log_lines.push_back( line );
+
+   std::string& line_one = log_lines[0];
+   std::size_t pos = line_one.find( "<" );
+   std::string expected_string = line_one.substr( pos );
+
+   std::vector< std::string > results;
+   std::string stream_str = stream.str();
+   boost::split( results, stream_str, boost::is_any_of( "\n" )  );
+   results.pop_back();
+
+   // Setting std::cout back to normal
+   std::cout.rdbuf( buf );
+
+   BOOST_REQUIRE_EQUAL( "<warning>: test", expected_string );
+
+   BOOST_REQUIRE_EQUAL( results.size(), logtypes.size() );
+   for ( int i = 0; i < results.size(); i++ )
+   {
+      pos = results[i].find( "<" );
+      std::string expected_result = results[i].substr( pos );
+      BOOST_REQUIRE_EQUAL( logtypes[i] + ": test", expected_result );
+   }
+
+   boost::log::core::get()->remove_all_sinks();
+}
+
 BOOST_AUTO_TEST_SUITE_END()

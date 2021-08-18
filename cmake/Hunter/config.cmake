b19c8345a8bbac21cb178c74a1d38e4e22c9d0b7
Steve Gerbino <steve@gerbino.co>
2021-08-18 04:55:58 -0400

    Use standardized HunterGate

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,14 +1,15 @@
 hunter_config(Boost
-   VERSION ${HUNTER_Boost_VERSION}
+   VERSION "1.72.0-p1"
    CMAKE_ARGS
-   USE_CONFIG_FROM_BOOST=ON
-   Boost_USE_STATIC_LIBS=ON
-   Boost_NO_BOOST_CMAKE=ON
+      USE_CONFIG_FROM_BOOST=ON
+      Boost_USE_STATIC_LIBS=ON
+      Boost_NO_BOOST_CMAKE=ON
 )
 
 hunter_config(koinos_util
-   URL "https://github.com/koinos/koinos-util-cpp/archive/32f7589037c6ebcb0c55f2dc558df44ed06176a0.tar.gz"
-   SHA1 "958a61b9316556382ac6300f7a40798c9e375040"
+   URL  "https://github.com/koinos/koinos-util-cpp/archive/2316ed57cae22170cca5a759a8b539261ac5a6ab.tar.gz"
+   SHA1 "db7b2112c2db1e3366ebdd9f773b997ec27694b9"
    CMAKE_ARGS
       BUILD_TESTS=OFF
 )
+
